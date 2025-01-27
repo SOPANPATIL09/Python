c7a6d3ef8487ecfc166f2df7cadd6167d82f88e0
sopan pramod patil <143089038+SOPANPATIL09@users.noreply.github.com>
2025-01-27 19:33:02 +0530

    Add files via upload

---
+++ PositiveorNegative.py
@@ -0,0 +1,18 @@
+"""Given an integer number, find if number is positive, negative or 0.
+If number is positive, print "Positive"
+If number is negative, print "Negative"
+And if number is equal to 0, print "Zero".
+"""
+
+number=int(input("enter a number: "))
+
+if(number>0):
+    print("number is positive")
+
+elif(number<0):
+    print("number is negative")
+
+else:
+    print("number is zero")
+    
+        
