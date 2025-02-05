e1e354040da4414b3cb6461e058dbf6e6e91ba11
sopan pramod patil <143089038+SOPANPATIL09@users.noreply.github.com>
2025-02-05 22:58:06 +0530

    Create function2.py
    #write a function to print of length of list

---
+++ function2.py
@@ -0,0 +1,11 @@
+#write a function to print of length of list
+
+cities=["mumbai","pune","nashik","jalgaon"]
+movies=["animal","pushpa","javan","ramayan"]
+
+def print_len(list):
+    for item in list:
+        print(item,end=" ")
+print_len(cities)
+print_len(movies)
+ 
