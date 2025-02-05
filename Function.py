6d9ded58dad06f6cd003de7830ff5eeaf77d99a1
sopan pramod patil <143089038+SOPANPATIL09@users.noreply.github.com>
2025-02-05 22:40:57 +0530

    Create Function.py
    even or odd number using function

---
+++ Function.py
@@ -0,0 +1,9 @@
+#even or odd number using function  
+num=int(input("enter a number"))
+def EvenOdd(num):
+    if num%2==0:
+        print("Even ",num)
+    else:
+        print("Odd ",num)    
+        
+EvenOdd(num)        
